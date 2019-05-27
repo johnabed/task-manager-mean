ee009cc19e6135f6973286d0d4108ac23e40d749
John Abed <john.abed@hotmail.com>
2019-05-27 13:15:50 -0400

    Task View visualized on front end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TaskViewComponent} from "./pages/task-view/task-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TaskViewComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
